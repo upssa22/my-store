c7503b472d817831b464a4b15c161728608c3c45
upssa22 <upssa.22@gmail.com>
2026-03-29 10:22:28 +0300

    Update app-config.js

--- js/app-config.js
+++ js/app-config.js
@@ -3,6 +3,7 @@ const CONFIG = {
     // بيانات الربط مع Supabase
     SUPABASE_URL: "https://roljlpmrgkuzeumviobn.supabase.co",
     SUPABASE_KEY: "sb_publishable_mEf2kXqJsivTvismRbM8cQ_RGeuii9q",
+    TELEGRAM_TOKEN: "8697542479:AAFEWJMoFlKqSx68yM5lLA165iYMYOF12vc"
 
     // إعدادات المتجر العامة
     STORE_NAME: "متجر ون بيس",
